d6b99968839ac39fbf3a06af31245a2518862443
Sebastian <sebastianmb88@gmail.com>
2024-06-06 15:50:15 -0500

    Conexion con api desde el front

--- wep-app/dropit-front/src/components/Booking/AutoCompleteAddres.jsx
+++ wep-app/dropit-front/src/components/Booking/AutoCompleteAddres.jsx
@@ -13,22 +13,19 @@ function AutoCompleteAddres() {
         return () => clearTimeout(delayDebounceFn)
     }, [source])
 
-    // URL de la API de Mapbox Search Box
-    const apiUrl = 'https://api.mapbox.com/search/searchbox/suggest';
-    const accessToken = 'pk.eyJ1IjoiY3Zhczg4IiwiYSI6ImNsd3dib2t0NTExNmIyam9sengyeWdwcncifQ.fS0NzZmL-920aprwyKoKmA'; // Reemplaza con tu propio token de acceso de Mapbox
-    // Construye la URL completa
-    const fullUrl = `${apiUrl}?q=${source}&access_token=${accessToken}`;
+ 
     const getAddressList = async () => {
-        const res = await fetch(fullUrl, {
-            headers: {
-                "Content-Type": "application/json",
+        const res = await fetch('http://localhost:3001/api/mapbox-suggestions?q='+source,{
+            headers:{
+                "Content-Type":"application/json",
             }
         });
 
-        const result = await res.json();
+        const result=await res.json();
         setAddressList(result)
 
     }
+
     return (
         <div className='mt-5'>
             <div>
@@ -39,6 +36,12 @@ function AutoCompleteAddres() {
                     value={source}
                     onChange={(e) => setSource(e.target.value)}
                 />
+                 {addressList?.suggestions?
+                <div>
+                    {addressList?.suggestions.map((item, index) =>
+                        <h2>{item.full_address}</h2>
+                    )}
+                </div>:null}
             </div>
             <div className='mt-3'>
                 <label className='text-gray-400'>Where To?</label>
@@ -46,12 +49,7 @@ function AutoCompleteAddres() {
                     className='bg-white p-1 border-[1px] w-full rounded-md outline-none
                             focus:border-cyan-900'
                 />
-                {addressList?.suggestions?
-                <div>
-                    {addressList?.suggestions.map((item, index) =>
-                        <h2>{item.full_address}</h2>
-                    )}
-                </div>:null}
+               
             </div>
         </div>
     )
