da8bca3d2594a6401702f3f453927a131c0abe83
Sebastian <sebastianmb88@gmail.com>
2024-07-03 22:37:03 -0500

    Consumiengo api google service

--- wep-app/dropit-front/src/components/Booking/AutoCompleteAddres.jsx
+++ wep-app/dropit-front/src/components/Booking/AutoCompleteAddres.jsx
@@ -2,34 +2,16 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import locationIcon from '../../assets/images/location.png'; // Ruta al archivo SVG del icono de ubicación
 import destinationIcon from '../../assets/images/destination.png'; // Ruta al archivo SVG del icono de destino
-
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function AutoCompleteAddres() {
 
-    const [source, setSource] = useState('');
-    const [addressList, setAddressList] = useState([])
-
-    useEffect(() => {
-        const delayDebounceFn = setTimeout(() => {
-            getAddressList()
-        }, 3000)
-        return () => clearTimeout(delayDebounceFn)
-    }, [source])
+    const [value, setValue] = useState(null);
+    
+    const googleApiKey = import.meta.env.VITE_GOOGLE_MAPS_APIKEY;
 
 
-    const getAddressList = async () => {
-        const res = await fetch('http://localhost:3001/api/mapbox-suggestions?q=' + source, {
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-
-        const result = await res.json();
-        setAddressList(result)
-        // Muestra la información en la consola
-        console.log('Direcciones obtenidas:', result);
-
-    }
+    
 
     return (
         <div className='mt-5'>
@@ -39,20 +21,40 @@ function AutoCompleteAddres() {
                             focus:border-cyan-900'>
                     <img src={locationIcon} alt="Ubicación" className="h-4 w-4" />
 
-                    <input type="text"
+                    {/*<input type="text"
                         className='bg-transparent w-full outline-none'
                         value={source}
                         onChange={(e) => setSource(e.target.value)}
+                    />*/}
+                    <GooglePlacesAutocomplete
+                        apiKey={googleApiKey}
+                        selectProps={{
+                            value,
+                            onChange: setValue,
+                            placeholder:'Pickup Location',
+                            isClearable:true,
+                            className:'w-full',
+                            components:{
+                                DropdownIndicator:false
+                            },
+                            styles:{
+                                control:(provide)=>({
+                                    ...provide,
+                                    backgroundColor:'white',
+                                    border:'none'
+                                })
+                            }
+                          }}
                     />
                 </div>
-                {addressList?.suggestions ?
+                {/*addressList?.suggestions ?
                     <div className='shadow-md p-1 rounded-md absolute w-full bg-white'>
                         {addressList?.suggestions.map((item) =>
                             <h2 className='p-3 hover:bg-gray-100 cursor-pointer'
                                 onClick={() => { setSource(item.full_address); setAddressList([]) }}
                                 key={item.mapbox_id}>{item.full_address}</h2>
                         )}
-                    </div> : null}
+                    </div> : null*/}
             </div>
             <div className='mt-3'>
                 <label className='text-gray-400'>Where To?</label>
