1625e45c233f2e73e879b2a8d3b8e38951093d46
Sebastian <sebastianmb88@gmail.com>
2024-10-08 21:52:53 -0500

    Configurando Auth en el back

--- wep-app/dropit-back/server/index.js
+++ wep-app/dropit-back/server/index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const { ClerkExpressWithAuth } = require('@clerk/clerk-sdk-node');
 const connectDB = require('../config/db');
 const orderRoutes = require('../routes/orderRoutes');
 
@@ -14,6 +15,9 @@ const orderRoutes = require('../routes/orderRoutes');
 
 
 app.use(express.json()); // Middleware para parsear JSON
+const clerkMiddleware = ClerkExpressWithAuth({
+  secretKey: process.env.VITE_CLERK_PUBLISHABLE_KEY
+});
 
 require('dotenv').config();
 
@@ -23,6 +27,24 @@ connectDB();
 app.use(cors());
 // Usar las rutas de pedidos
 app.use('/api/orders', orderRoutes);
+
+
+// Use the lax middleware that returns an empty auth object when unauthenticated
+app.get(
+  '/panel',
+  clerkMiddleware({
+    // Add options here
+    // See the Middleware options section for more details
+  }),
+  (req, res) => {
+    res.json(req.auth)
+  },
+)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(401).send('Unauthenticated!')
+})
 
 const PORT = process.env.PORT || 3001;
 
